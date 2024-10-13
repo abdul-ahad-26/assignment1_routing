0aa3d2d8f70df2399c3c4e8c5083bdb9118002f4
abdulahad_26 <hayymalik678@gmail.com>
2024-10-13 15:50:42 +0500

    asasad

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Dynamic routing</h1>
+      <Link href="/country">Country</Link>
     </div>
   )
 }
